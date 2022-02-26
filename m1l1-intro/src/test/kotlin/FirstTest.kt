1ec4e7410b5ee7220e05097f9653ebe3ca9a4c74
Pavel Pavlov <pavel.pavlov@lamoda.ru>
2022-02-26 21:19:17 +0300

    Initial commit

---
+++ m1l1-intro/src/test/kotlin/FirstTest.kt
@@ -0,0 +1,10 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class FirstTest {
+
+    @Test
+    fun `first test`() {
+        assertEquals(3, 2 + 1)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,4 @@
+rootProject.name = "ok-202202-marketplace"
+include("m1l1")
+include("m1l1-intro")
+include("m1l1-intro")
